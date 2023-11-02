d9f0e9ce06abd4445246369fbfa2833e1cab9148
Nathan Dickinson <nathandickinson04@gamil.com>
2023-11-02 17:25:57 -0400

    I added my complete addition to generate a random password using if statements to determine the users wants and a for loop to randomize the password and created arrays to pull from.

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,9 @@
+//My variables
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -7,8 +13,82 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+//My Code
+function generatePassword() {
 
+  var passwordLength = window.prompt("How many characters would you like your password? Choose a number betweeen 8 and 128");
+ 
+ if (!passwordLength) {
+   alert("Required Value");
+ } else if (passwordLength < 8 || passwordLength > 128) {
+   passwordLength = window.prompt("You must choose a number between 8 and 128");
+ } else { 
+   var confirmLower = window.confirm("Click OK if you would like lower case letters");
+   var confirmUpper = window.confirm("Click OK if you would like upper case letters");
+   var confirmNumber = window.confirm("Click OK if you would like any numbers");
+   var confirmSpecial = window.confirm("Click OK if you would like any special characters");
+ };
+ 
+ if (!confirmLower && !confirmNumber && !confirmSpecial && !confirmUpper) {
+   window.alert("You must select at least 1")
+ }
+ else if (confirmLower && confirmNumber && confirmSpecial && confirmUpper) {
+   var userchoice = lowerCase.concat(special, numbers, upperCase)
+ } 
+ else if (confirmLower && confirmNumber && confirmSpecial) {
+   userchoice = lowerCase.concat(special, numbers)
+ } 
+ else if (confirmLower && confirmNumber && confirmUpper) {
+   userchoice = lowerCase.concat(numbers, upperCase)
+ }
+ else if (confirmLower && confirmSpecial && confirmUpper) {
+   userchoice = lowerCase.concat(special, upperCase)
+ }
+ else if (confirmNumber && confirmSpecial && confirmUpper) {
+   userchoice = special.concat(numbers, upperCase)
+ }
+ else if (confirmLower && confirmNumber) {
+   userchoice = lowerCase.concat(numbers)
+ }
+ else if (confirmLower && confirmUpper) {
+   var userchoice = lowerCase.concat(upperCase)
+ }
+ else if (confirmLower && confirmSpecial) {
+   var userchoice = lowerCase.concat(special)
+ }
+ else if (confirmNumber && confirmSpecial) {
+   var userchoice = numbers.concat(special)
+ }
+ else if (confirmSpecial && confirmUpper) {
+   var userchoice = special.concat(upperCase)
+ }
+ else if (confirmNumber && confirmUpper) {
+   var userchoice = numbers.concat(upperCase)
+ }
+ else if (confirmLower) {
+   var userchoice = lowerCase
+ }
+ else if (confirmNumber) {
+   var userchoice = numbers
+ }
+ else if (confirmSpecial) {
+   var userchoice = special
+ }
+ else if (confirmUpper) {
+   var userchoice = upperCase
+ };
+
+ var randomPassword = ""
+      
+ for (var i = 0; i < passwordLength; i++) {
+   randomPassword = randomPassword + userchoice[Math.floor(Math.random() * userchoice.length)];
+ }
+ return randomPassword;
+
+ };
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
